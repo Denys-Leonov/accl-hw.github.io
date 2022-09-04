de41e0845a4203caaf39e2f6f7288fc4bfa1951e
Denys-Leonov <skydark912@gmail.com>
2022-09-04 12:26:36 +0300

    build

--- src/MyItemList/MyItemList.jsx
+++ src/MyItemList/MyItemList.jsx
@@ -5,12 +5,12 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import './MyItemList.css'
+import './MyItemList.css';
 
 function MyItemList({ dataArr }) {
   const styleObj = {
     accordion: { backgroundColor: 'rgba(240, 240, 13, 0.312)', m: '3px 0' },
-    typography: {
+    typographySummary: {
       display: 'flex',
       justifyContent: 'space-between',
       flexGrow: '1',
@@ -19,9 +19,12 @@ function MyItemList({ dataArr }) {
     },
     idSection: { flex: '0 0 15%' },
     summarySection: { margin: '0 5px', flex: '1 1 auto' },
-    assigneeSectionHide: { flex: '0 0 20%', display: {xs: 'none', sm: 'none', md: 'block'} },
-    assigneeSectionShow: { display: {xs: 'block', sm: 'block', md: 'none'} },
-    divider: { m: '5px 0'}
+    assigneeSectionHide: {
+      flex: '0 0 20%',
+      display: { xs: 'none', sm: 'none', md: 'block' },
+    },
+    assigneeSectionShow: { display: { xs: 'block', sm: 'block', md: 'none' } },
+    divider: { m: '5px 0' },
   };
 
   return (
@@ -30,21 +33,28 @@ function MyItemList({ dataArr }) {
         {dataArr.map((item) => (
           <Accordion disableGutters key={item['uuid']} sx={styleObj.accordion}>
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography sx={styleObj.typography}>
-                <span style={styleObj.idSection}>ID: {item['Issue key']}</span>
-                <span style={styleObj.summarySection} >
+              <Typography sx={styleObj.typographySummary}>
+                <Typography component='span' sx={styleObj.idSection}>ID: {item['Issue key']}</Typography>
+                <Typography component='span' sx={styleObj.summarySection}>
                   Summary: {item.Summary}
-                </span>
-                {/* <span style={styleObj.assigneeSection} className='assigneeHide'> */}
-                <Typography sx={styleObj.assigneeSectionHide} >
+                </Typography>
+                <Typography component='span' sx={styleObj.assigneeSectionHide}>
                   Assignee: {item.Assignee}
                 </Typography>
               </Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography>Summary: {item.Summary}</Typography>
-              
-              <Typography sx={styleObj.assigneeSectionShow}><Divider sx={styleObj.divider}/>Assignee: {item.Assignee}</Typography>
+              <Typography>Description: {item.Description}</Typography>
+              <Divider sx={styleObj.divider} />
+              <Typography>Issue type: {item['Issue Type']}</Typography>  
+              <Divider sx={styleObj.divider} />
+              <Typography>Creator: {item.Creator}</Typography>
+              <Divider sx={styleObj.divider} />
+              <Typography>Created: {item.Created}</Typography>
+              <Divider sx={[styleObj.divider, styleObj.assigneeSectionShow]} />
+              <Typography sx={styleObj.assigneeSectionShow}>
+                Assignee: {item.Assignee}
+              </Typography>
             </AccordionDetails>
           </Accordion>
         ))}
